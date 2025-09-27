a06ab1cadff69d9be30a5cb0827465cb3dc0b07d
Enos Andrade <enosads@gmail.com>
2025-09-27 01:47:39 -0300

    Add SplashView

---
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashView.swift
+//  Reminder
+//
+//  Created by Enos Andrade on 27/09/25.
+//
+
+import Foundation
---
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewController.swift
+//  Reminder
+//
+//  Created by Enos Andrade on 27/09/25.
+//
+
+import Foundation
